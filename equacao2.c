a3cc8a1a5d17f3c9a444213d26a0be44478ab8c9
Gabriel Dutra <gabrieldutra@decom.cefetmg.br>
2013-01-16 17:57:52 -0200

    Calculador de equaçao 2o grau

---
+++ equacao2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+
+    int valora=0,valorb=0,valorc=0,deseja=0;
+    printf("Digite o valor de a: ");
+    scanf("%d", &valora);	
+    printf("Digite o valor de b: ");
+    scanf("%d", &valorb);
+    printf("Digite o valor de c: ");
+    scanf("%d", &valorc);	
+    printf("Calculando...");
+    int delta=valorb*valorb-4*valora*valorc;
+    int raizdelta=sqrt(delta);
+    printf("O valor de delta é: %d e sua raíz é %d \n",delta,raizdelta);
+    printf("Calculando os valores de x...\n");
+    int x=(-valorb+raizdelta)/2*valora;
+    int xx=(-valorb-raizdelta)/2*valora;
+    printf("Os 2 valores de x são %d e %d...\n",x,xx);
+    printf("Deseja calcular novamente(0=NAO/1=SIM)? ");
+    scanf("%d", &deseja);  
+   if(deseja == 1) main();
+    printf("\n");	 
+}
